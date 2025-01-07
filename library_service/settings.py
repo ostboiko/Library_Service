65352eb1af0d62db672cf347b556a1fe96ad9977
Ostap Boiko <138430404+ostboiko@users.noreply.github.com>
2025-01-07 18:56:33 +0200

    add borrowings_service in INSTALLED_APPS

--- library_service/settings.py
+++ library_service/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'books_service',
+    'book_service',
+    'borrowings_service',
 ]
 
 MIDDLEWARE = [
