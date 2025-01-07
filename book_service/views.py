99384c45a96a5a773ddde21ac9fba6589d8ae5a4
Ostap Boiko <138430404+ostboiko@users.noreply.github.com>
2025-01-07 18:53:51 +0200

    add BookViewSet in views.py

--- book_service/views.py
+++ book_service/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 
 from book_service.models import Book
+from book_service.serializers import BookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.object.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
