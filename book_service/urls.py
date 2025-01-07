7524b4133539f457b1acb9e05dc49ac6813b16fd
Ostap Boiko <138430404+ostboiko@users.noreply.github.com>
2025-01-07 18:55:48 +0200

    add book_service/urls.py

---
+++ book_service/urls.py
@@ -0,0 +1,12 @@
+from rest_framework import routers
+from book_service.views import BookViewSet
+
+
+router = routers.DefaultRouter()
+router.register("books", BookViewSet)
+
+
+urlpatterns = router.urls
+
+
+app_name = "book-service"
