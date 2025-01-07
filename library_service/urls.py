32a417e18796edbaedde87f9073693e5994d0a2d
Ostap Boiko <138430404+ostboiko@users.noreply.github.com>
2025-01-07 18:56:07 +0200

    add library-service/urls.py

--- library_service/urls.py
+++ library_service/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/book-service/", include("book_service.urls", namespace="book-service")),
 ]
