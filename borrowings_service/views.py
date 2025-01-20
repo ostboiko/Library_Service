799a4c08959bf26bf850a294c5070af5e7e15dd3
Ostap Boiko <138430404+ostboiko@users.noreply.github.com>
2025-01-20 22:15:09 +0200

    add views.py

--- borrowings_service/views.py
+++ borrowings_service/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, exceptions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from datetime import datetime
 
-# Create your views here.
+from book_service.models import Book
+from borrowings_service.models import Borrowing
+from borrowings_service.serializers import (
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+    BorrowingCreateSerializer,
+    BorrowingReturnSerializer
+)
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.select_related("user", "book")
+    permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self):
+        user = self.request.user
+        is_active = self.request.query_params.get("is_active")
+        queryset = self.queryset
+        user_id = self.request.query_params.get("user_id", None)
+
+        if is_active:
+            queryset = queryset.filter(actual_return_date__isnull=True)
+
+        if user.is_superuser or user.is_staff:
+            if user_id:
+                queryset = queryset.filter(user=user_id)
+            return queryset
+
+        return queryset.filter(user=user)
+
+    def get_serializer_class(self):
+
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+
+        return BorrowingListSerializer
+
+    @action(
+        methods=["PATCH"],
+        detail=True,
+        url_path="return-borrowing",
+        permission_classes=[IsAuthenticated, ],
+    )
+    def return_borrowing(self, request, pk=None):
+
+        try:
+            borrowing = Borrowing.objects.get(pk=pk)
+        except Borrowing.DoesNotExist:
+            raise exceptions.NotFound("Borrowing with this ID does not exist.")
+
+        if borrowing.actual_return_date:
+            raise ValueError("Borrowing can't be return more than once!")
+
+        serializer = BorrowingReturnSerializer(borrowing, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            book = borrowing.book
+            book.inventory += 1
+            book.save()
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(
+        methods=["POST"],
+        detail=False,
+        url_path="create-borrowing",
+        permission_classes=[IsAuthenticated, ],
+    )
+    def create_borrowing(self, request):
+        serializer = BorrowingCreateSerializer(data=request.data)
+        book_id = request.data.get("book")
+        book = Book.objects.get(id=book_id)
+
+        if serializer.is_valid():
+            book.inventory -= 1
+            book.save()
+            serializer.save(user=self.request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
