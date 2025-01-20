71613143a5d486841646670a2404636742ccd7d3
Ostap Boiko <138430404+ostboiko@users.noreply.github.com>
2025-01-20 22:04:19 +0200

    create models.py

--- borrowings_service/models.py
+++ borrowings_service/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from library_service.settings import AUTH_USER_MODEL
+from book_service.models import Book
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True, null=False, blank=False)
+    expected_return_date = models.DateField(null=False, blank=False)
+    actual_return_date = models.DateField(null=True, blank=True)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings_service")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings_service")
+
+    def __str__(self):
+        return f"{self.user.email} - {self.book.title}"
